9130634d02a76c0cb0adfa08c8a1389edfa06cc5
Reed Jones <reedjones@reedjones.com>
2019-06-07 12:35:03 -0600

    Update macos-aliases.plugin.zsh

--- macos-aliases.plugin.zsh
+++ macos-aliases.plugin.zsh
@@ -2,6 +2,7 @@ export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH="$HOME/.fastlane/bin:$PATH"
 export HOMESTEAD_DIR="$HOME/Homestead"
 
 
